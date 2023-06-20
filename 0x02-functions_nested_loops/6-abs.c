affbcac2d03c4aed034074d10a3e6dad727c0bc0
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-20 16:13:42 -0700

    absolute of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_abs - absolute value
+ *@n:checking character
+ *Return:n if positive
+ *if not multipy -n
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else 
+	{
+		return (-n);
+	}
+}
