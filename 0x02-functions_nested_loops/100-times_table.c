3127fff5de7574dac49f90794d4b5e9520678a4a
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-20 19:03:31 -0700

    print n time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *print_times_table - print table
+ *@n:time table
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(48);
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				k = i * j;
+					if (k <= 99)
+						_putchar(' ');
+				if (k <= 9)
+					_putchar(' ');
+				if (k >= 100)
+				{
+					_putchar((k / 100) + 48);
+					_putchar((k / 10) % 10 + 48);
+				}
+				else if (k <= 99 && k >= 10)
+				{
+					_putchar((k / 10) + 48);
+				}
+				_putchar((k & 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
