22472339a038b0eb0307171b06057814f0130280
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-20 18:13:43 -0700

    adds of tow number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - adds of tow numbers
+ *@a:integer number
+ *@b:integer number
+ *Return: result of c
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
