969a2a3284a394d9620ba656d200955ba852af00
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-20 17:59:16 -0700

    print 9 time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *times_table - print 9 time table
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = b * a;
+			if (b  == 0)
+			{
+				_putchar(c + 48);
+			}
+			if (c < 10 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + 48);
+			}
+			else if (c >= 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
