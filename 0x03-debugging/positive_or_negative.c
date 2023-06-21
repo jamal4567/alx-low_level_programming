57696b9551cf270128c361947414738f632c00f6
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-21 13:39:51 -0700

    create 0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *positive_or_negative - test number
+ *@i:for testing
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive \n", i);
+	else if (i < 0)
+		printf("%d is negative \n", i);
+	else
+		printf("%d is zero \n", i);
+}
