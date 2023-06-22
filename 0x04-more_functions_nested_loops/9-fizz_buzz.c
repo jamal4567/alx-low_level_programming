a58eaacd1bfb79e7e0b94bd82e04ba3c27ce539d
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-22 13:57:20 -0700

    FizzBuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/******/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s","FizzBuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s","Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s","Fizz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
