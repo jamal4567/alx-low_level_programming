c85eedded7602d25ae1eeeb808980ad02e76b272
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-22 10:26:15 -0700

    print 10 time

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *more_numbers - print 10 time
+ *
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10 ) + '0');
+			}
+			_putchar((j % 10 ) + '0');
+			j++;
+		}
+		_putchar('\n');
+	}
+}
