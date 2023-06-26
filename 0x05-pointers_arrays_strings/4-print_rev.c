ab873a83dbe9db43098ab0a3b5c08018d0343b48
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-26 13:46:47 -0700

    print in revers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_rev - print string in reverse
+ *
+ *@s: string to print
+ */
+void print_rev(char *s)
+{
+	int i;
+	int length;
+
+	i = 0;
+	for (length = 0; s[length] != '\0'; length++)
+	{	
+	}
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
