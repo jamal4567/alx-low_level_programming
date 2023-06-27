db0d67941282e9c96517116fbf4922a551d98e1a
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-26 18:32:24 -0700

    convert a string to an integer.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_atoi - convert string to integer
+ *@s:string
+ *Return:the converted integer
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+
+	while (s[0] != '\0')
+	{
+		if (s[0] == '-')
+			sign *= - 1;
+		else if (s[0] >= '0' && s[0] <= '9')
+			result = (result * 10) + (s[0] - '0') * sign;
+		else if (result)
+			break;
+		s++;
+	}
+	return (result);
+}
