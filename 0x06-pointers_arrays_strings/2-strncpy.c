555b48127973d1521e8b2c844618976e9c5e9c78
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-29 04:21:05 -0700

    copies a string.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ **_strncpy -  copies a string.
+ *@dest:string1
+ *@src:string2
+ *@n:numbers of bytes
+ *Return:dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
