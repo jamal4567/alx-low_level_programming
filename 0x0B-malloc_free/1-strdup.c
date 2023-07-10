bc97bcea78c6b1e385d6557fe44f7ab0be6e906a
jamalimmiss <jamalimmiss97@gmail.com>
2023-07-10 11:06:32 -0700

    returns a pointer to a newly

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ **_strdup -  pointer to a newly allocated space
+ *
+ *@str: string
+ *
+ *Return: result
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *Nstr;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (str == NULL)
+		return (NULL);
+	Nstr = (char *)malloc(sizeof(str) * (i + 1));
+	if (Nstr == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+	{
+		Nstr[j] = str[j];
+	}
+	return (Nstr);
+}
