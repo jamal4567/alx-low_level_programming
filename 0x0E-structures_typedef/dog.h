f8bc7d60daea84ea395536a65cfacac634f74b32
jamalimmiss <jamalimmiss97@gmail.com>
2023-07-17 16:45:54 -0700

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ *struct dog - dog informations
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
