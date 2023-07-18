b58bd520a15887b7a4e82d6806f69d3ca7d6729e
jamalimmiss <jamalimmiss97@gmail.com>
2023-07-17 18:31:26 -0700

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**********/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog;
+
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+	(*ndog).name = name;
+	(*ndog).age = age;
+	(*ndog).owner = owner;
+	return (ndog);
+}
