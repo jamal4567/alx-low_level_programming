a3b832e875a08fcef5b667a310e2144282a41136
jamalimmiss <jamalimmiss97@gmail.com>
2023-07-17 18:34:53 -0700

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
-/**********/
+/**
+ **new_dog - create new dog
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ *
+ *Return: new_dog
+ *
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
