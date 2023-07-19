03d9d75b387e2f417c4b4cf93fdfb483ca93b5fa
jamalimmiss <jamalimmiss97@gmail.com>
2023-07-19 11:44:49 -0700

    TASK 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,23 +7,25 @@
  */
 int main(int argc, char **argv)
 {
-	int (*op)(int, int);
-	int result;
+	int (*operator)(int, int);
+	int calc, num1, num2;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	op = get_op_func(argv[2]);
+	operator = get_op_func(argv[2]);
 
-	if (op == NULL)
+	if (!operator)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = op(atoi(argv[1]), atoi(argv[3]));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	calc = operator(num1, num2);
 
-	printf("%d\n", result);
+	printf("%d\n", calc);
 	return (0);
 }
