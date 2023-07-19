960c7b94c21a7e2750615dd7890ceee67cc3c677
jamalimmiss <jamalimmiss97@gmail.com>
2023-07-19 07:59:48 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ *print_name - print name
+ *@name: name
+ *
+ *@f:function pointer
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
