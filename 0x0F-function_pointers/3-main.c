cf1ffd0581d6f0dceeb3f5249d8503a3c24d4a2d
jamalimmiss <jamalimmiss97@gmail.com>
2023-07-19 12:09:33 -0700

    TASK 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,6 +15,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	operator = get_op_func(argv[2]);
 
 	if (!operator)
