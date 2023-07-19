cd33f8e58dd5d315a501b21e4fb30f9d41f1aef2
jamalimmiss <jamalimmiss97@gmail.com>
2023-07-19 12:23:23 -0700

    TASK 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * struct op - Struct op
  *
@@ -22,4 +23,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
