4df729065e1a447665bd71a5ed76983bf44891c6
jamalimmiss <jamalimmiss97@gmail.com>
2023-07-20 16:40:28 -0700

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_all(const char * const format, ...)
 				str = va_arg(args, char *);
 				if (!str)
 					printf("(nil)");
-				printf("%s%s",separator ,str);
+				printf("%s%s", separator, str);
 				break;
 			default:
 				i++;
