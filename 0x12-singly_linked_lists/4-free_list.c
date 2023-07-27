a1617cfcc3f503eca3f86b1e41d9f853017f0383
jamal <jamalimmiss97@gmail.com>
2023-07-27 11:58:14 -0700

    task-4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_list -  frees a list_t list.
+ *@head: freed list list_t
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
