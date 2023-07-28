9f9844b3b1d0558ebb87b64a13866e3a52d6d0f6
jamal <jamalimmiss97@gmail.com>
2023-07-27 18:00:48 -0700

    task-5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ *bemain -execute befor main function
+ */
+void __attribute__((constructor)) bemain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
