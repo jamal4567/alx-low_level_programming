030765303bdd128cff63ef41c772d5ef52349ddf
jamal <jamalimmiss97@gmail.com>
2023-08-01 18:31:36 -0700

    task-10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *next;
+	listint_t *next = NULL;
 	listint_t *current = *head;
 
 	if (current == NULL)
