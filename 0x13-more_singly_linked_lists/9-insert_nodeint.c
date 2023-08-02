8dd59953500ef8069f2c426ee68f416ef676d9fc
jamal <jamalimmiss97@gmail.com>
2023-08-01 17:56:47 -0700

    task-9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		tmp->next = *head;
 		*head = tmp;
-		return (tmp)
+		return (tmp);
 	}
 	tmp->next = h->next;
 	h->next = tmp;
