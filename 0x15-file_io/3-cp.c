836b49f8ede684f1ab0418d4fd5d6f572495c3cf
jamal <jamalimmiss97@gmail.com>
2023-08-07 17:29:25 -0700

    task-3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,11 +15,11 @@ int main(int ac, char **av)
 	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); }
 	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
+	if (file_from < 0)
 	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98); }
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | 0664);
-	if (file_to == -1)
+	if (file_to < 0)
 	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99); }
 	while ((nb = read(file_from, buffer, 1024)) > 0)
@@ -28,19 +28,19 @@ int main(int ac, char **av)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-	if (nb == -1)
+	if (nb < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	file_from = close(file_from);
 	file_to = close(file_to);
-	if (file_from)
+	if (file_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	if (file_to)
+	if (file_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
