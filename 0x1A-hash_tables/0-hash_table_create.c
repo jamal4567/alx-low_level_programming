98f2e3227a966c067919d23fc35614b2a16029d1
jamal97 <jamalimmmiss97@gmail.com>
2023-10-06 06:54:17 -0700

    task-0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	h->size = size;
-	h->array = malloc(sizeof(hash_table_t) * size);
+	h->array = malloc(sizeof(hash_table_t *) * size);
 	if (!h->array)
 		return (NULL);
 	for (i = 0; i < size; i++)
